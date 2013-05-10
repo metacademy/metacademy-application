2db08c3f9b3a4a228df2739ddb565045ac1dc36c
Roger Grosse <rgrosse@mit.edu>
2013-05-10 18:01:10 -0400

    updated config.py template

--- config-template.py
+++ config-template.py
@@ -4,6 +4,7 @@ TEMP_PATH = '/path/to/tmp-area'
 HASH = 'randomLettersAndNumbers'
 CONTENT_SERVER_PORT = 9090
 FRONTEND_SERVER_PORT = 8080
+INDEX_PATH = '/path/to/index'      # directory to build the index in
 
 #  hack to obtain ports in bash script
 if __name__ == "__main__":

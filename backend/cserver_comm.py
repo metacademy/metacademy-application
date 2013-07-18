6f4d66aeddf83bcd1e49c0609516e4127a9f182b
Colorado Reed <colorado.j.reed@gmail.com>
2013-07-19 00:01:32 +0100

    basic working search functionality between django and content server

---
+++ backend/cserver_comm.py
@@ -0,0 +1,24 @@
+from backend.settings import CONTENT_SERVER
+import urllib2
+import urllib
+import json
+import pdb
+
+def get_search_json(search_term):
+    """
+    search the content server for the specified search_term
+    """
+    url_req = urllib2.Request("%s/search?q=%s" % (CONTENT_SERVER, urllib.quote_plus(search_term)))
+    url_hand = urllib2.urlopen(url_req)
+    try:
+        sobj = json.load(url_hand)
+    except ValueError:
+        sobj = None
+        print 'Error parsing json for content server search: %s\n returning None' % search_term
+    return sobj
+
+def is_node_present(tag):
+    """
+    Check if the node is present on the content server TODO
+    """
+    pass
--- backend/views.py
+++ backend/views.py
@@ -1,13 +1,16 @@
+import config
+import cserver_comm
 from backend import settings
 from backend.db_handler import db
-import config
-from content_server import formats
-from forms import ResourceForm
 from backend.settings import CONTENT_SERVER
+from content_server import formats
+
+from forms import ResourceForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, redirect, render
 from django.template import RequestContext
 from django import forms
+
 import os
 import pdb
 import urllib
@@ -30,6 +33,12 @@ def get_search_view(request):
     """
     Returns the search (list) view for a given query
     """
+    qstring = request.GET['q']
+    if len(qstring) == 0:
+        search_data = None
+        print 'WARNING: empty query parameter in get_search_view'
+    else:
+        search_data = cserver_comm.get_search_json(qstring)
     
-    return render_to_response('search-results.html', {'content_server':CONTENT_SERVER}, context_instance=RequestContext(request))
+    return render_to_response('search-results.html', {'content_server':CONTENT_SERVER, 'search_data': search_data}, context_instance=RequestContext(request))
 

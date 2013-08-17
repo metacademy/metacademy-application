a35c1f5f739b1cef66d8ad79fa1789727f16f301
Colorado Reed <colorado.j.reed@gmail.com>
2013-08-17 22:37:04 +0100

    added PRODUCTION global flag to agfk & piwik analytics tracking

--- app_server/static/javascript/gen-utils.js
+++ app_server/static/javascript/gen-utils.js
@@ -48,6 +48,20 @@ function genFun($){
           if (input.val() == input.attr('placeholder')) {
             input.val('');
           }
+
+          // piwik tracking
+          if (window.PRODUCTION){
+            var _paq = _paq || [];
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u=(("https:" == document.location.protocol) ? "https" : "http") + "://metacademy.org/analytics/piwik//";
+              _paq.push(['setTrackerUrl', u+'piwik.php']);
+              _paq.push(['setSiteId', 1]);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
+              g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+            })();
+          }
         });
       });
     }

cd8e3c235db2f4ed1af06e66e61c41f2d8e4cb3a
Colorado Reed <colorado.j.reed@gmail.com>
2013-08-23 00:41:16 +0100

    added basic "about" page

--- app_server/urls.py
+++ app_server/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 # handle static files locally
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.decorators.cache import cache_page
+from django.views.generic.base import TemplateView
 
 from apps.graph.views import benchmark_viewer
 
@@ -16,8 +17,9 @@ urlpatterns = patterns('',
                        url(r'^', include('apps.content_search.urls') ),
                        url(r'^(?i)graphs/', include('apps.graph.urls', namespace="graphs") ),
                        url(r'^user/', include('apps.user_management.urls', namespace='user') ),
-                       url(r'^dev/benchmarktest', benchmark_viewer),
+                       # url(r'^dev/benchmarktest', benchmark_viewer),
                        url(r'^captcha/', include('captcha.urls')),
+                       url(r'^about/',  TemplateView.as_view(template_name='about.html'))
 )
 
 urlpatterns += staticfiles_urlpatterns()

9306f8c5392004bbe16eed0871d4fe156e40da62
Colorado Reed <colorado.j.reed@gmail.com>
2013-08-31 20:28:37 -0700

    added simple redirect for /concepts/concept_name to /graphs/concepts/concept_name

--- app_server/urls.py
+++ app_server/urls.py
@@ -2,19 +2,16 @@ from django.conf.urls import patterns, include, url
 # handle static files locally
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.decorators.cache import cache_page
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
 
 from apps.graph.views import benchmark_viewer
 
 """
-Django urls handler. Valid URLS/Requests:
-  kmap(s)                           display knowledge-map browser
-
-see simple_server for valid content requests
+Django urls handler
 """
-
 urlpatterns = patterns('',
                        url(r'^', include('apps.content_search.urls') ),
+                       url(r'^(?i)concepts/((?P<anything>.*))', RedirectView.as_view(url="/graphs/concepts/%(anything)s", query_string=True), name='concepts'),
                        url(r'^(?i)graphs/', include('apps.graph.urls', namespace="graphs") ),
                        url(r'^user/', include('apps.user_management.urls', namespace='user') ),
                        url(r'^roadmaps/', include('apps.roadmaps.urls', namespace='roadmaps')),

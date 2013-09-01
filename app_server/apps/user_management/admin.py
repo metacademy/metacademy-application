d395c4e8b15a1b9c8c7bc5f707b29892662a83c4
Colorado Reed <colorado.j.reed@gmail.com>
2013-09-01 14:40:43 -0700

    adjusted learned concepts display for admin interface

--- app_server/apps/user_management/admin.py
+++ app_server/apps/user_management/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+from django.db.models import Count
 
 from apps.user_management.models import Profile, LearnedConcept
 
@@ -12,10 +13,20 @@ class UserProfileInline(admin.StackedInline):
 class UserAdmin(AuthUserAdmin):
     inlines = [UserProfileInline]
 
-class LearnedConceptAdmin:
-    pass
+class LearnedConceptAdmin(admin.ModelAdmin):
+    list_display = ('get_title', 'show_uprofile_count',)
+    filter_horizontal = ('uprofiles',)
+    readonly_fields = ('get_title', 'show_uprofile_count')
+    fields = ('get_title', 'show_uprofile_count', 'uprofiles',)
+    
+    def queryset(self, request):
+        return LearnedConcept.objects.annotate(uprofiles_count=Count('uprofiles'))
 
+    def show_uprofile_count(self, inst):
+        return inst.uprofiles_count
+    show_uprofile_count.admin_order_field = 'uprofiles_count'
+        
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
-admin.site.register(LearnedConcept)
+admin.site.register(LearnedConcept, LearnedConceptAdmin)
 

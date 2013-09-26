f6f68f5df9f1366f7cd3fd4a36ae02d05470bd33
Colorado Reed <colorado.j.reed@gmail.com>
2013-09-25 17:14:53 -0700

    added starredconcept to admin

--- app_server/apps/user_management/admin.py
+++ app_server/apps/user_management/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 from django.db.models import Count
 
-from apps.user_management.models import Profile, LearnedConcept
+from apps.user_management.models import Profile, LearnedConcept, StarredConcept
 
 class UserProfileInline(admin.StackedInline):
     model = Profile
@@ -13,20 +13,29 @@ class UserProfileInline(admin.StackedInline):
 class UserAdmin(AuthUserAdmin):
     inlines = [UserProfileInline]
 
-class LearnedConceptAdmin(admin.ModelAdmin):
+class ConceptAdmin(admin.ModelAdmin):
     list_display = ('get_title', 'show_uprofile_count',)
     filter_horizontal = ('uprofiles',)
     readonly_fields = ('get_title', 'show_uprofile_count')
     fields = ('get_title', 'show_uprofile_count', 'uprofiles',)
     
     def queryset(self, request):
-        return LearnedConcept.objects.annotate(uprofiles_count=Count('uprofiles'))
+        return 0
 
     def show_uprofile_count(self, inst):
         return inst.uprofiles_count
     show_uprofile_count.admin_order_field = 'uprofiles_count'
+
+class LearnedConceptAdmin(ConceptAdmin):
+    def queryset(self, request):
+        return LearnedConcept.objects.annotate(uprofiles_count=Count('uprofiles'))
+
+class StarredConceptAdmin(ConceptAdmin):
+    def queryset(self, request):
+        return StarredConcept.objects.annotate(uprofiles_count=Count('uprofiles'))
         
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.register(LearnedConcept, LearnedConceptAdmin)
+admin.site.register(StarredConcept, StarredConceptAdmin)
 

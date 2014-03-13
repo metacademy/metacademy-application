0a086a2f2453926c7a73360fff6014b15ed01d17
Colorado Reed <colorado.j.reed@gmail.com>
2014-03-13 04:09:53 -0700

    fixed concept display list

--- app_server/views.py
+++ app_server/views.py
@@ -22,8 +22,22 @@ multiple apps or are app agnostic
 def get_list_view(request):
     """
     Return the list of concepts
+
+		TODO this should be cached
     """
-    return render(request, "concept-list.html", {"concepts": Concept.objects.extra(select={'lower_title': 'lower(title)'}).order_by("lower_title")})
+    citms = []
+    # previous starting letter
+    prev_sl = ""
+    for concept in Concept.objects.extra(select={'lower_title': 'lower(title)'}).order_by("lower_title"):
+        if concept.is_provisional() or len(concept.title) == 0:
+            continue
+        sl = concept.title[0].upper()
+        if sl != prev_sl:
+            citms.append(sl)
+            prev_sl = sl
+        citms.append(concept)
+
+    return render(request, "concept-list.html", {"citms": citms})
 
 
 class MultiSearchView(SearchView):

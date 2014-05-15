cb3aef8c5a2717e4062f9f938a3a52f7df24f8f9
Colorado Reed <colorado.j.reed@gmail.com>
2014-05-14 18:04:48 -0700

    removed content server from travis.yml and tests.sh

--- Tests.sh
+++ Tests.sh
@@ -3,9 +3,6 @@
 # set the env
 source ../meta_venv/bin/activate
 
-# start the servers
-# python app_server/manage.py runserver 8080 &
-# rserver_id=$!
 
 # run the django tests
 echo "Django Tests"
@@ -17,7 +14,3 @@ echo "Browser Tests"
 
 # run the selenium tests
 python selenium_tests/simple_selenium_tests.py
-
-# clean up
-# pkill -TERM -P $rserver_id
-# kill `ps aux|grep manage.py|awk '{print ($2)}'`
